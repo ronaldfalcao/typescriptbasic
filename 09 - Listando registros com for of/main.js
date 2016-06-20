5058ff5910f463c7e288bf41a041ae86e44e26ab
ronaldfalcao <ronald@ronaldfalcao.com.br>
2016-06-20 11:54:17 -0300

    Listagem de registros com for of em typescript

---
+++ 09 - Listando registros com for of/main.js
@@ -0,0 +1,18 @@
+var cores = ["Amarelo", "Verde", "Azul", "Vermelho", "Laranja"];
+for (var i in cores) {
+    console.log(i);
+}
+for (var _i = 0, cores_1 = cores; _i < cores_1.length; _i++) {
+    var cor = cores_1[_i];
+    console.log(cor);
+}
+var frutas = ["Laranja", "Maça", "Pêra", "Limão", "Melância"];
+frutas['title'] = "Frutas";
+for (var i in frutas) {
+    console.log(i);
+}
+for (var _a = 0, frutas_1 = frutas; _a < frutas_1.length; _a++) {
+    var fruta = frutas_1[_a];
+    console.log(fruta);
+}
+//# sourceMappingURL=main.js.map
---
+++ 09 - Listando registros com for of/main.ts
@@ -0,0 +1,21 @@
+var cores = ["Amarelo", "Verde", "Azul", "Vermelho", "Laranja"];
+
+for (var i in cores){
+	console.log(i);
+}
+
+for (var cor of cores){
+	console.log(cor);
+}
+
+var frutas = ["Laranja", "Maça", "Pêra", "Limão", "Melância"];
+
+frutas['title'] = "Frutas";
+
+for (var i in frutas) {
+	console.log(i);
+}
+
+for (var fruta of frutas) {
+	console.log(fruta);
+}
